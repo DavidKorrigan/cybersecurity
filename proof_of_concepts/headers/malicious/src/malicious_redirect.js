e4ca8786d33d1c7329fe933c8cd7f18aec60455c
The Tipsy Korrigan <d.michaud@protonmail.com>
2023-08-03 09:00:48 +0200

    Update malicious_redirect.js

--- proof_of_concepts/headers/malicious/src/malicious_redirect.js
+++ proof_of_concepts/headers/malicious/src/malicious_redirect.js
@@ -1,1 +1,1 @@
-alert('XSS');
+window.location.href='https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/07-Input_Validation_Testing/02-Testing_for_Stored_Cross_Site_Scripting';
